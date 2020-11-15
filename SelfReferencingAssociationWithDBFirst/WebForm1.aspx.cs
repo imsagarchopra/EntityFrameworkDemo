c27a330d8aa03388382cb9a7b163254ed8b5af19
Sagar Chopra <schopra@firemate.com>
2020-11-15 19:05:40 +0530

    Self Referencing Association

---
+++ SelfReferencingAssociationWithDBFirst/WebForm1.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SelfReferencingAssociationWithDBFirst
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            EmployeeDBContext employeeDBContext = new EmployeeDBContext();
+
+            GridView1.DataSource = employeeDBContext.Employeees.Select(emp => new {
+                EmployeeName = emp.EmployeeName,
+                ManagerName = emp.Manager == null ? "Super Boss" : emp.Manager.EmployeeName
+            }).ToList();
+
+            GridView1.DataBind();
+
+        }
+    }
+}
