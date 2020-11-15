89fbfaa25e6703a40f2e206c0fbec09f95a74c03
Sagar Chopra <schopra@firemate.com>
2020-11-15 16:37:08 +0530

    StoredProcedures, Entity Splitting

---
+++ StoredProceduresWithEFCodeFirstApproach/EmployeeDBContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace StoredProceduresWithEFCodeFirstApproach
+{
+    public class EmployeeDBContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // This line will tell entity framework to use stored procedures
+            // when inserting, updating and deleting Employees
+            modelBuilder.Entity<Employee>().MapToStoredProcedures
+             (p => p.Insert(i => i.HasName("InsertEmployee")
+                                     .Parameter(n => n.Name, "EmployeeName")
+                                     .Parameter(n => n.Gender, "EmployeeGender")
+                                     .Parameter(n => n.Salary, "EmployeeSalary"))
+                     .Update(u => u.HasName("UpdateEmployee")
+                                     .Parameter(n => n.ID, "EmployeeID")
+                                     .Parameter(n => n.Name, "EmployeeName")
+                                     .Parameter(n => n.Gender, "EmployeeGender")
+                                     .Parameter(n => n.Salary, "EmployeeSalary"))
+                     .Delete(d => d.HasName("DeleteEmployee")
+                                     .Parameter(n => n.ID, "EmployeeID"))
+             );
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ StoredProceduresWithEFCodeFirstApproach/EmployeeRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StoredProceduresWithEFCodeFirstApproach
+{
+    public class EmployeeRepository
+    {
+        EmployeeDBContext employeeDBContext = new EmployeeDBContext();
+
+        public List<Employee> GetEmployees()
+        {
+            return employeeDBContext.Employees.ToList();
+        }
+
+        public void InsertEmployee(Employee employee)
+        {
+            employeeDBContext.Employees.Add(employee);
+            employeeDBContext.SaveChanges();
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            var employeeToUpdate = employeeDBContext.Employees.FirstOrDefault(x => x.ID == employee.ID);
+            employeeToUpdate.Name = employee.Name;
+            employeeToUpdate.Gender = employee.Gender;
+            employeeToUpdate.Salary = employee.Salary;
+
+            employeeDBContext.SaveChanges();
+        }
+
+        public void DeleteEmployee(Employee employee)
+        {
+            var employeeToDelete = employeeDBContext.Employees.FirstOrDefault(x => x.ID == employee.ID);
+            employeeDBContext.Employees.Remove(employeeToDelete);
+
+            employeeDBContext.SaveChanges();
+        }
+    }
+}
