c12cf54d07d55a03364e9c0a1cd4ac86223a3b39
Sagar Chopra <schopra@firemate.com>
2020-11-17 15:05:51 +0530

    Table per hierarchy Inheritance

---
+++ TablePerHierarchyInheritanceWithCodeFirst/ContractEmployee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TablePerHierarchyInheritanceWithCodeFirst
+{
+    public class ContractEmployee : Employee
+    {
+        [Column(Order = 6)]
+        public int HoursWorked { get; set; }
+        [Column(Order = 7)]
+        public int HourlyPay { get; set; }
+    }
+}
---
+++ TablePerHierarchyInheritanceWithCodeFirst/PermanentEmployee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TablePerHierarchyInheritanceWithCodeFirst
+{
+    public class PermanentEmployee : Employee
+    {
+        [Column(Order = 5)]
+        public int AnnualSalary { get; set; }
+    }
+}
